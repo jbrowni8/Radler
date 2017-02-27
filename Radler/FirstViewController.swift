9d94c5fe74ff3f38a31baa51ed21916b8ec57b31
James Browning <jbxmv@mail.missouri.edu>
2017-02-27 15:26:43 -0500

    Initial Commit

---
+++ Radler/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Radler
+//
+//  Created by James Browning on 2/27/17.
+//  Copyright © 2017 James Browning. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
